58daf7f13238d491640cbbbec28265d1acf62d25
Anjelina Gulla <anjelina.sertse@gmail.com>
2019-01-19 00:29:03 -0500

    Fixes duplicate restaurant operating hours table.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,16 +1,5 @@
 let restaurant;
 var newMap;
-
-/**
- * Initialize map as soon as the page is loaded.
- */
-document.addEventListener('DOMContentLoaded', (event) => {  
-  initMap();
-});
-
-/**
- * Initialize leaflet map
- */ 
  
  window.initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
